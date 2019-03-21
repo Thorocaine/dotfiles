a64830032ff73502d321f80a09cbd651a43e877e
Jonathan Peel <jonathan@polymorphic.group>
2019-03-21 18:44:39 +0200

    add muttrc template

--- bootsrap/bootstrap.sh
+++ bootsrap/bootstrap.sh
@@ -5,8 +5,12 @@ cd ~/.dotfiles
 git clone https://github.com/nojhan/liquidprompt.git
 source ~/.dotfiles/liquidprompt/liquidprompt
 
+#mutt profile
+cp ~/.dotfiles/mutt/muttrc.template ~/.dotfiles/mutt/muttrc
+
 #Sym Links
 ln -s ~/.dotfiles/config     ~/.config
 ln -s ~/.dotfiles/bashrc     ~/.bashrc
+ln -s ~/.ditfiles/mutt       ~/.mutt
 ln -s ~/.dotfiles/Xresources ~/.Xresources
 ln -s ~/.dotfiles/xinitrc    ~/.xinitrc
