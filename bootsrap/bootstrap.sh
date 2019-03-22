9c8e663a14ad18365e0e3ec3e51b024c3c7748ee
Jonathan Peel <jonathan@polymorphic.group>
2019-03-22 06:14:40 +0200

    add mutt setup

--- bootsrap/bootstrap.sh
+++ bootsrap/bootstrap.sh
@@ -6,11 +6,15 @@ git clone https://github.com/nojhan/liquidprompt.git
 source ~/.dotfiles/liquidprompt/liquidprompt
 
 #mutt profile
-cp ~/.dotfiles/mutt/muttrc.template ~/.dotfiles/mutt/muttrc
+# sudo touch /var/mail/me
+# sudo chmod 660 /var/mail/me
+# sudo chown `whoami`:mail /var/mail/me 
+cp ~/.dotfiles/mutt/template.igs ~/.dotfiles/mutt/account.igs
+cp ~/.dotfiles/mutt/template.me ~/.dotfiles/mutt/account.igs
 
 #Sym Links
 ln -s ~/.dotfiles/config     ~/.config
 ln -s ~/.dotfiles/bashrc     ~/.bashrc
-ln -s ~/.ditfiles/mutt       ~/.mutt
+ln -s ~/.dotfiles/mutt       ~/.mutt
 ln -s ~/.dotfiles/Xresources ~/.Xresources
 ln -s ~/.dotfiles/xinitrc    ~/.xinitrc
